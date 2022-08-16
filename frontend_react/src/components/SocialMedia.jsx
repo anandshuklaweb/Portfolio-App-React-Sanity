f55efded357ef52970c95214b4669fc4a44b975d
anandshuklaweb <anandshukla.web@gmail.com>
2022-08-16 17:48:12 +0530

    Portfolio App

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BsTwitter, BsInstagram, BsLinkedin } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+
+const SocialMedia = () => {
+  return (
+    <div className="app__social">
+      <div>
+        <BsLinkedin />
+      </div>
+      <div>
+        <BsTwitter />
+      </div>
+      <div>
+        <FaFacebookF />
+      </div>
+      <div>
+        <BsInstagram />
+      </div>
+    </div>
+  );
+};
+
+export default SocialMedia;
